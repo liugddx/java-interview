82f4b9260407c985d573ecbe61a875eed7148558
ethan.liu <ethan.liu@quectel.com>
2021-06-02 15:34:15 +0800

    fegin下载服务开发

--- src/main/java/com/liugddx/web/DemoController.java
+++ src/main/java/com/liugddx/web/DemoController.java
@@ -1,7 +1,16 @@
 package com.liugddx.web;
 
+import com.liugddx.client.FileClient;
+import feign.Response;
+import java.io.IOException;
+import java.io.InputStream;
 import javax.websocket.server.PathParam;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/5/26 15:07
  */
 @RestController
+@RequiredArgsConstructor
 public class DemoController {
 
+    private final FileClient fileClient;
     @Data
     static class Param{
         private String test;
@@ -44,6 +55,37 @@ public class DemoController {
     @GetMapping("/none")
     public String none(){
         return "none";
+    }
+
+
+    @GetMapping("/download")
+    public ResponseEntity<byte[]> download(){
+        ResponseEntity<byte[]> result = null;
+        InputStream inputStream = null;
+        try {
+            // feign文件下载
+            Response response = fileClient.download();
+            Response.Body body = response.body();
+            inputStream = body.asInputStream();
+            byte[] b = new byte[inputStream.available()];
+            inputStream.read(b);
+            HttpHeaders heads = new HttpHeaders();
+            heads.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=lr.xls");
+            heads.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+            result = new ResponseEntity<byte[]>(b, heads, HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
     }
 
 }
